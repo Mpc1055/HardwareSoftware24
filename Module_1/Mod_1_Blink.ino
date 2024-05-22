85ff768cdedcf5a24fd5962589d5be74f4c8865e
Mpc1055 <michael.caiozzo@gmail.com>
2024-05-22 11:36:43 -0400

    mod 1

---
+++ Module_1/Mod_1_Blink.ino
@@ -0,0 +1,15 @@
+/* Summer Robotics Camp Code 1 "Blink"
+    Written by Michael Caiozzo 
+*/
+// Variable Declarations
+
+int Led = 13;
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
